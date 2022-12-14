131e824b46138bcd16fa4f794b6e341cf3cd17a2
NoorAlBattashi <nooralbattashi97@gmail.com>
2022-12-15 00:04:47 +0400

    latest update

--- Practice/src/PracticeFile.java
+++ Practice/src/PracticeFile.java
@@ -10,7 +10,7 @@
 import java.util.Scanner;
 
 public class PracticeFile {
-	
+
 	private static void printBoard(String[][] board) {
 		System.out.println();
 		System.out.println(board[0][0] + "  |  " + board[0][1] + "  |  " + board[0][2]);
@@ -21,11 +21,48 @@ public class PracticeFile {
 		System.out.println();
 	}
 
+	private static boolean checkWinner(String[][] board, String symbol) {
+		boolean playerWon = false;
+		if (board[0][0] == symbol && board[0][1] == symbol && board[0][2] == symbol
+				|| board[1][0] == symbol && board[1][1] == symbol && board[1][2] == symbol
+				|| board[2][0] == symbol && board[2][1] == symbol && board[2][2] == symbol
+				|| board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol
+				|| board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol
+				|| board[0][0] == symbol && board[1][0] == symbol && board[2][0] == symbol
+				|| board[0][1] == symbol && board[1][1] == symbol && board[2][1] == symbol
+				|| board[0][2] == symbol && board[1][2] == symbol && board[2][2] == symbol) {
+
+			// display the board
+			printBoard(board);
+
+			// display win message
+			System.out.println(symbol + ", You win !");
+			playerWon = true;
+		}
+		return playerWon;
+	}
+
+	private static boolean checkDraw(String[][] board) {
+		boolean Draw = false;
+		if (board[0][0] != "1" && board[0][1] != "2" && board[0][2] != "3" && board[1][0] != "4" && board[1][1] != "5"
+				&& board[1][2] != "6" && board[2][0] != "7" && board[2][1] != "8" && board[2][2] != "9") {
+
+			// display the board
+			printBoard(board);
+
+			// display win message
+			System.out.println("Draw, No winner !");
+			Draw = true;
+		}
+		return Draw;
+
+	}
+
 	/**
-	 * @param args : practice
+	 * @param args : practice TicTacToe Game
 	 */
 	public static void main(String[] args) {
-		// create array for the tictactoe positions
+		// create array for the TicTacToe positions
 		String[][] ticTacToeArr = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
 
 		// display the board
@@ -52,13 +89,16 @@ public class PracticeFile {
 			// player one turn
 			PlayerOneTurn = true;
 
+			boolean isThereAWinner = false;
+			boolean isDraw = false;
+
 			// LOOP TO CONTINUE THE GAME
-			while (validTurnsCounter < 9) {
+			while (validTurnsCounter < 9 && isThereAWinner == false && isDraw == false) {
 
 				// display the board
 				printBoard(ticTacToeArr);
-				
-				if (PlayerOneTurn == true) {
+
+				if (PlayerOneTurn == true && isThereAWinner == false && isDraw == false) {
 					// player 1 select move
 					System.out.print("Player 1, Make a move, Enter position: ");
 					Scanner player1Movesc = new Scanner(System.in);
@@ -66,7 +106,6 @@ public class PracticeFile {
 					// player1Movesc.close();
 					String player1Symbol = "X";
 					String player2Symbol = "O";
-				
 
 					// check the selection position of player1 and add value
 					if (player1Move == 1) {
@@ -152,43 +191,26 @@ public class PracticeFile {
 							System.out.println("The position already choosen");
 							PlayerOneTurn = true;
 						}
+
 					}
+
 					// if player 1 select unknown selection
 					else {
 						System.out.println("Wrong selection");
 						PlayerOneTurn = true;
 					}
 					// check winner
-					if (ticTacToeArr[0][0] == "X" && ticTacToeArr[0][1] == "X" && ticTacToeArr[0][2] == "X"
-							|| ticTacToeArr[1][0] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[1][2] == "X"
-							|| ticTacToeArr[2][0] == "X" && ticTacToeArr[2][1] == "X" && ticTacToeArr[2][2] == "X"
-							|| ticTacToeArr[0][0] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[2][2] == "X"
-							|| ticTacToeArr[0][2] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[2][0] == "X"
-							|| ticTacToeArr[0][0] == "X" && ticTacToeArr[1][0] == "X" && ticTacToeArr[2][0] == "X"
-							|| ticTacToeArr[0][1] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[2][1] == "X"
-							|| ticTacToeArr[0][2] == "X" && ticTacToeArr[1][2] == "X" && ticTacToeArr[2][2] == "X") {
-
-						// display the board
-						printBoard(ticTacToeArr);
-						
-						// display win message
-						System.out.println("Player 1, you win !");
-						break;
+					if (isThereAWinner == false) {
+						isThereAWinner = checkWinner(ticTacToeArr, player1Symbol);
+						checkWinner(ticTacToeArr, player1Symbol);
 					}
-					// Draw
-					else if (ticTacToeArr[0][0] != "1" && ticTacToeArr[0][1] != "2" && ticTacToeArr[0][2] != "3"
-							&& ticTacToeArr[1][0] != "4" && ticTacToeArr[1][1] != "5" && ticTacToeArr[1][2] != "6"
-							&& ticTacToeArr[2][0] != "7" && ticTacToeArr[2][1] != "8" && ticTacToeArr[2][2] != "9") {
-
-						// display the board
-						printBoard(ticTacToeArr);
-						
-						// display win message
-						System.out.println("Draw, No winner !");
-						break;
+					// check Draw
+					if (isThereAWinner == false) {
+						isDraw = checkDraw(ticTacToeArr);
+						checkDraw(ticTacToeArr);
 					}
 
-					if (PlayerOneTurn == false) {
+					if (PlayerOneTurn == false && isThereAWinner == false && isDraw == false) {
 						// player 2 turn
 						System.out.print("Player 2, Make a move, Enter position: ");
 						Scanner player2Movesc = new Scanner(System.in);
@@ -281,36 +303,11 @@ public class PracticeFile {
 							System.out.println("Wrong selection");
 							PlayerOneTurn = false;
 						}
+
 						// check winner
-						if (ticTacToeArr[0][0] == "O" && ticTacToeArr[0][1] == "O" && ticTacToeArr[0][2] == "O"
-								|| ticTacToeArr[1][0] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[1][2] == "O"
-								|| ticTacToeArr[2][0] == "O" && ticTacToeArr[2][1] == "O" && ticTacToeArr[2][2] == "O"
-								|| ticTacToeArr[0][0] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[2][2] == "O"
-								|| ticTacToeArr[0][2] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[2][0] == "O"
-								|| ticTacToeArr[0][0] == "O" && ticTacToeArr[1][0] == "O" && ticTacToeArr[2][0] == "O"
-								|| ticTacToeArr[0][1] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[2][1] == "O"
-								|| ticTacToeArr[0][2] == "O" && ticTacToeArr[1][2] == "O"
-										&& ticTacToeArr[2][2] == "O") {
-
-							// display the board
-							printBoard(ticTacToeArr);
-							
-							// display win message
-							System.out.println("Player 2, you win !");
-							break;
-						}
-						// Draw
-						else if (ticTacToeArr[0][0] != "1" && ticTacToeArr[0][1] != "2" && ticTacToeArr[0][2] != "3"
-								&& ticTacToeArr[1][0] != "4" && ticTacToeArr[1][1] != "5" && ticTacToeArr[1][2] != "6"
-								&& ticTacToeArr[2][0] != "7" && ticTacToeArr[2][1] != "8"
-								&& ticTacToeArr[2][2] != "9") {
-
-							// display the board
-							printBoard(ticTacToeArr);
-							
-							// display win message
-							System.out.println("Draw, No winner !");
-							break;
+						if (isThereAWinner == false) {
+							isThereAWinner = checkWinner(ticTacToeArr, player2Symbol);
+							checkWinner(ticTacToeArr, player2Symbol);
 						}
 					}
 				}
@@ -325,19 +322,23 @@ public class PracticeFile {
 
 			// player one turn
 			PlayerOneTurn = true;
+			boolean isThereAWinner = false;
+			boolean isDraw = false;
 
 			// LOOP TO CONTINUE THE GAME
-			while (validTurnsCounter < 9) {
+			while (validTurnsCounter < 9 && isThereAWinner == false && isDraw == false) {
 
 				// display the board
 				printBoard(ticTacToeArr);
-				
-				if (PlayerOneTurn == true) {
+
+				if (PlayerOneTurn == true && isThereAWinner == false && isDraw == false) {
 					// player 1 select move
 					System.out.print("Player 1, Make a move, Enter position: ");
 					Scanner player1Movesc = new Scanner(System.in);
 					int player1Move = Integer.parseInt(player1Movesc.nextLine());
 					// player1Movesc.close();
+					String player1Symbol = "O";
+					String player2Symbol = "X";
 
 					// check the selection position of player1 and add value
 					if (player1Move == 1) {
@@ -430,36 +431,17 @@ public class PracticeFile {
 						PlayerOneTurn = true;
 					}
 					// check winner
-					if (ticTacToeArr[0][0] == "O" && ticTacToeArr[0][1] == "O" && ticTacToeArr[0][2] == "O"
-							|| ticTacToeArr[1][0] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[1][2] == "O"
-							|| ticTacToeArr[2][0] == "O" && ticTacToeArr[2][1] == "O" && ticTacToeArr[2][2] == "O"
-							|| ticTacToeArr[0][0] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[2][2] == "O"
-							|| ticTacToeArr[0][2] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[2][0] == "O"
-							|| ticTacToeArr[0][0] == "O" && ticTacToeArr[1][0] == "O" && ticTacToeArr[2][0] == "O"
-							|| ticTacToeArr[0][1] == "O" && ticTacToeArr[1][1] == "O" && ticTacToeArr[2][1] == "O"
-							|| ticTacToeArr[0][2] == "O" && ticTacToeArr[1][2] == "O" && ticTacToeArr[2][2] == "O") {
-
-						// display the board
-						printBoard(ticTacToeArr);
-						
-						// display win message
-						System.out.println("Player 1, you win !");
-						break;
+					if (isThereAWinner == false) {
+						isThereAWinner = checkWinner(ticTacToeArr, player1Symbol);
+						checkWinner(ticTacToeArr, player1Symbol);
 					}
-					// Draw
-					else if (ticTacToeArr[0][0] != "1" && ticTacToeArr[0][1] != "2" && ticTacToeArr[0][2] != "3"
-							&& ticTacToeArr[1][0] != "4" && ticTacToeArr[1][1] != "5" && ticTacToeArr[1][2] != "6"
-							&& ticTacToeArr[2][0] != "7" && ticTacToeArr[2][1] != "8" && ticTacToeArr[2][2] != "9") {
-
-						// display the board
-						printBoard(ticTacToeArr);
-						
-						// display win message
-						System.out.println("Draw, No winner !");
-						break;
+					// check Draw
+					if (isThereAWinner == false) {
+						isDraw = checkDraw(ticTacToeArr);
+						checkDraw(ticTacToeArr);
 					}
 
-					if (PlayerOneTurn == false) {
+					if (PlayerOneTurn == false && isThereAWinner == false && isDraw == false) {
 						// player 2 turn
 						System.out.print("Player 2, Make a move, Enter position: ");
 						Scanner player2Movesc = new Scanner(System.in);
@@ -553,35 +535,9 @@ public class PracticeFile {
 							PlayerOneTurn = false;
 						}
 						// check winner
-						if (ticTacToeArr[0][0] == "X" && ticTacToeArr[0][1] == "X" && ticTacToeArr[0][2] == "X"
-								|| ticTacToeArr[1][0] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[1][2] == "X"
-								|| ticTacToeArr[2][0] == "X" && ticTacToeArr[2][1] == "X" && ticTacToeArr[2][2] == "O"
-								|| ticTacToeArr[0][0] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[2][2] == "X"
-								|| ticTacToeArr[0][2] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[2][0] == "X"
-								|| ticTacToeArr[0][0] == "X" && ticTacToeArr[1][0] == "X" && ticTacToeArr[2][0] == "X"
-								|| ticTacToeArr[0][1] == "X" && ticTacToeArr[1][1] == "X" && ticTacToeArr[2][1] == "X"
-								|| ticTacToeArr[0][2] == "X" && ticTacToeArr[1][2] == "X"
-										&& ticTacToeArr[2][2] == "X") {
-
-							// display the board
-							printBoard(ticTacToeArr);
-							
-							// display win message
-							System.out.println("Player 2, you win !");
-							break;
-						}
-						// Draw
-						else if (ticTacToeArr[0][0] != "1" && ticTacToeArr[0][1] != "2" && ticTacToeArr[0][2] != "3"
-								&& ticTacToeArr[1][0] != "4" && ticTacToeArr[1][1] != "5" && ticTacToeArr[1][2] != "6"
-								&& ticTacToeArr[2][0] != "7" && ticTacToeArr[2][1] != "8"
-								&& ticTacToeArr[2][2] != "9") {
-
-							// display the board
-							printBoard(ticTacToeArr);
-							
-							// display win message
-							System.out.println("Draw, No winner !");
-							break;
+						if (isThereAWinner == false) {
+							isThereAWinner = checkWinner(ticTacToeArr, player2Symbol);
+							checkWinner(ticTacToeArr, player2Symbol);
 						}
 					}
 				}
